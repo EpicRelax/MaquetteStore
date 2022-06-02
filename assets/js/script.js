0dda4ed0b6e786906135792964260f5ee5046cc5
EpicRelax <epicrelax76@gmail.com>
2022-06-02 16:47:33 +0200

    Add fullScreen Img in js on cards

---
+++ assets/js/script.js
@@ -0,0 +1,34 @@
+const imgElements = document.querySelectorAll(".cards img");
+const frameImg = document.createElement("div");
+const imgFull = [];
+
+frameImg.id = "frameImg";
+frameImg.style.width = "100vw";
+frameImg.style.height = "100vh";
+frameImg.style.backdropFilter = "blur(5px)";
+frameImg.style.backgroundColor = "rgba(0, 0, 0, 0.5)"
+frameImg.style.display = "none";
+frameImg.style.justifyContent = "center";
+frameImg.style.alignItems = "center";
+frameImg.style.position = "fixed";
+frameImg.style.zIndex = "2";
+document.body.prepend(frameImg);
+
+let i = 0;
+while (i < imgElements.length) {
+    let n = i;
+    imgElements[i].addEventListener("click", function(){
+        frameImg.style.display = "flex";
+        imgFull[n] = document.createElement("img");
+        imgFull[n].src = imgElements[n].src;
+        frameImg.append(imgFull[n]);
+    });
+    i++;
+}
+
+frameImg.addEventListener("click", function(event){
+    if (!frameImg.querySelector("img").contains(event.target)) {
+        frameImg.style.display = "none";
+        frameImg.innerHTML ="";
+    }
+})
